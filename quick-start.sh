5918582bfc07dffed37420bf194816672c1fd359
Kyle Blair <github.freeware771@passmail.net>
2025-10-02 22:01:35 -0700

    feat: make script better

--- quick-start.sh
+++ quick-start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ command -v npm >/dev/null 2>&1 ]; then
+	echo "npm is installed"
+else
+	echo "npm is not installed. Please install Node.js and npm first."
+	exit 1
+fi
 export port=10000
 export insecure=true
 npm start
