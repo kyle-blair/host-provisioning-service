85a901ddaabb459fcd4ae90c9dea69f87ff7ba48
Kyle Blair <github.freeware771@passmail.net>
2025-10-03 19:43:02 +0000

    fix: npm check

--- quick-start.sh
+++ quick-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ command -v npm >/dev/null 2>&1 ]; then
+if command -v npm >/dev/null 2>&1; then
 	echo "npm is installed"
 else
 	echo "npm is not installed. Please install Node.js and npm first."
